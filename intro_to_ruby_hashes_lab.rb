f834e7a37690adfdc73f0487e4b922b06a038d14
Devin Kelly <devin.cloud.kelly@gmail.com>
2019-10-06 17:56:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :rent_in_dollars => {
-	      :names => 10,
+	      :key_1 => 10,
 	      :key_2 => 200,
 	      :key_3 => 400, 
 	      :key_4 => 600
