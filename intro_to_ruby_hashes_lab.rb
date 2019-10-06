b1dd96b0acd4cfc5ec5f8cfa4898fa62376496b9
Devin Kelly <devin.cloud.kelly@gmail.com>
2019-10-06 17:59:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,9 +28,9 @@ def monopoly_with_third_tier
 	    :pieces => 4,
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
-	      :two_pieces_owned => 200,
-	      :two_pieces_owned => 400, 
-	      :two_pieces_owned => 600
+	      :two_pieces_owned => 50,
+	      :three_pieces_owned => 75, 
+	      :four_pieces_owned => 100
 	    },
 	    :names => {
 	      :first_player => 'bob',
